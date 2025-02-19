cafaabe9b59b3812588b3a7b47f88f05833a7219
angelynelau <48088108+angelynelau@users.noreply.github.com>
2025-02-19 22:32:37 +0800

    Update jbalb_p2s1.py

--- jbalb_p2s1.py
+++ jbalb_p2s1.py
@@ -164,7 +164,7 @@ if st.button("Generate Report"):
         pmnt_report += "FITTING = \n"
     
     pmnt_report += "DELIVERY = " + pipe_size + "\n"
-    pmnt_report += join(delivery_entries) + "\n"
+    pmnt_report += .join(delivery_entries) + "\n"
 
     if morning_weather == afternoon_weather:
         pmnt_report += f"WEATHER = {morning_weather}\n"
