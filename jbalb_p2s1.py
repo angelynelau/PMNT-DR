31d26807604445177564a2fc261964af743a7ebd
angelynelau <48088108+angelynelau@users.noreply.github.com>
2025-02-19 21:54:13 +0800

    Update jbalb_p2s1.py

--- jbalb_p2s1.py
+++ jbalb_p2s1.py
@@ -1,5 +1,4 @@
 import streamlit as st
-import urllib.parse
 from datetime import datetime, time
 
 def format_chainage(value):
@@ -31,9 +30,9 @@ working_time = f"{start_time.strftime('%H%M')}-{end_time.strftime('%H%M')} hrs"
 
 # MACHINERY Selection
 st.markdown("**MACHINERY**")
-machinery_types = []
+machinery_list = []
 if st.checkbox("Excavator"):
-    machinery_types.append("Excavator - 1")
+    machinery_list.append("Excavator - 1")
 
 # EQUIPMENT Selection
 st.markdown("**EQUIPMENT**")
@@ -59,34 +58,6 @@ if st.checkbox("General Worker"):
     team_members.append(f"General Worker - {workers}")
     total_people += workers
 
-# Materials Delivered
-st.markdown("**MATERIALS DELIVERED TO SITE**")
-materials = []
-pipe_entries = []
-total_pipe_length = 0
-delivery_entries = []  # Fixing the undefined variable issue
-
-if st.checkbox("Pipe"):
-    num_entries = st.number_input("Number of Pipe Entries", min_value=1, step=1, value=1)
-    for i in range(num_entries):
-        pipe_count = st.number_input(f"Pipe Count (Entry {i+1})", min_value=0, step=1, key=f"pipe_count_{i}")
-        route = st.text_input(f"Route (Entry {i+1})", key=f"route_{i}")
-        chainage_input = st.text_input(f"Chainage (Entry {i+1})", key=f"chainage_{i}")
-        chainage = format_chainage(chainage_input) if chainage_input else ""
-        total_pipe_length += pipe_count
-        
-        if pipe_count > 0 and route and chainage:
-            pipe_entries.append(f"- {pipe_count} lengths // {route} {chainage}")
-            delivery_entries.append(f"- {pipe_count} lengths // {route} {chainage}")
-
-    if pipe_entries:
-        materials.append(f"1. {pipe_size}\n" + "\n".join(pipe_entries))
-    else:
-        materials.append(f"1. {pipe_size}\n- {total_pipe_length} lengths")
-
-if st.checkbox("Valves & Fittings"):
-    materials.append(f"{len(materials)+1}. Valves & Fittings")
-
 # ACTIVITY CARRIED OUT
 st.markdown("**ACTIVITY CARRIED OUT**")
 activity_list = []
@@ -97,17 +68,16 @@ if st.checkbox("Pipe Laying"):
     if start_chainage and end_chainage:
         start_formatted = format_chainage(start_chainage)
         end_formatted = format_chainage(end_chainage)
-        if start_formatted and end_formatted:
-            chainage_length = f"({int(end_chainage) - int(start_chainage)}m)"
-            activity_list.append(f"{len(activity_list)+1}. Pipe Laying \n- {pipe_size} pipe laying works from {start_formatted} to {end_formatted} {chainage_length}")
+        chainage_length = f"({int(end_chainage) - int(start_chainage)}m)"
+        activity_list.append("Pipe Laying")
 
 if st.checkbox("Pipe Jointing"):
     joint_count = st.number_input("Number of Joints", min_value=0, step=1)
     joint_route = st.text_input("Insert Route")
     joint_chainage = format_chainage(st.text_input("Jointing Chainage"))
     if joint_count and joint_chainage:
-        activity_list.append(f"{len(activity_list)+1}. Pipe Jointing \n- {joint_count} nos joints ({pipe_size}) // {joint_route}-{joint_chainage}")
-    
+        activity_list.append("Pipe Jointing")
+
 # REMARKS
 remarks = st.text_area("REMARKS")
 
@@ -120,49 +90,61 @@ if st.button("Generate Report"):
     jbalb_report += f"Afternoon: {afternoon_weather}\n"
     jbalb_report += f"Total Working Hours: {working_hours:.2f} hrs\n"
     jbalb_report += f"{working_time}\n\n"
-    
-    if machinery_types:
-        jbalb_report += "*MACHINERY*\n" + "\n".join(machinery_types) + "\n\n"
-    
+
+    if machinery_list:
+        jbalb_report += "*MACHINERY*\n" + "\n".join(machinery_list) + "\n\n"
+
     if equipment_list:
         jbalb_report += "*EQUIPMENT*\n" + "\n".join(equipment_list) + "\n\n"
-    
+
     if team_members:
         jbalb_report += "*PIPE LAYING TEAM*\n" + "\n".join(team_members) + "\n\n"
-    
-    if materials:
-        jbalb_report += "*MATERIALS DELIVERED TO SITE*\n" + "\n".join(materials) + "\n\n"
-    
+
     if activity_list:
-        jbalb_report += "*ACTIVITY CARRIED OUT*\n" + "\n".join(activity_list) + "\n\n"
-    
+        jbalb_report += "*ACTIVITY CARRIED OUT*\n"
+        if start_chainage and end_chainage:
+            jbalb_report += f"- Pipe Laying {pipe_size} from {start_formatted} to {end_formatted} {chainage_length}\n"
+        if joint_count:
+            jbalb_report += f"- Pipe Jointing {pipe_size}, {joint_count} joints at {joint_chainage}\n"
+        jbalb_report += "\n"
+
     if remarks:
-        jbalb_report += "*REMARKS*\n" + remarks + "\n"    
+        jbalb_report += "*REMARKS*\n" + remarks + "\n"
 
     # PMNT FORMAT
     pmnt_report = f"> {team}\n"
     pmnt_report += f"PIPE = {pipe_size}\n"
     pmnt_report += f"DATE = {formatted_date}\n"
     
+    # Ensure proper WORK ACTIVITY format
     if activity_list:
         pmnt_report += f"WORK ACTIVITY = {pipe_size} {' & '.join(activity_list)}\n"
     else:
         pmnt_report += "WORK ACTIVITY = \n"
-    
+
     pmnt_report += f"HOURS WORKING = {working_hours:.2f} hrs ({working_time})\n"
     pmnt_report += f"MANPOWER = {total_people}\n"
     pmnt_report += f"JOINT = {joint_count}\n"
 
     if start_chainage and end_chainage:
-        pmnt_report += f"LAID = {start_chainage} to {end_chainage} {chainage_length}\n" 
+        pmnt_report += f"LAID = {start_chainage} to {end_chainage} {chainage_length}\n"
     else:
         pmnt_report += "LAID = \n"
 
     pmnt_report += "DELIVERY = " + pipe_size + "\n"
-    pmnt_report += "\n".join(delivery_entries) + "\n\n"
-    
+
+    if morning_weather == afternoon_weather:
+        pmnt_report += f"WEATHER = {morning_weather}\n"
+    else:
+        pmnt_report += f"MORNING WEATHER AM = {morning_weather}\n"
+        pmnt_report += f"AFTERNOON WEATHER PM = {afternoon_weather}\n"
+
     if remarks:
         pmnt_report += f"REMARKS = {remarks}\n"
 
-    st.text_area("JBALB Format Report:", jbalb_report, height=300)
-    st.text_area("PMNT Format Report:", pmnt_report, height=300)
+    # Display Reports
+    st.subheader("JBALB Format Report:")
+    st.text_area("JBALB Report", jbalb_report, height=300)
+
+    st.subheader("PMNT Format Report:")
+    st.text_area("PMNT Report", pmnt_report, height=300)
