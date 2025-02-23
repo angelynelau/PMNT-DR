efe132e75f90f01f1d41d177f75e3915443d24c3
angelynelau <48088108+angelynelau@users.noreply.github.com>
2025-02-23 21:27:55 +0800

    Update PMNTSR.py

--- PMNTSR.py
+++ PMNTSR.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import urllib.parse
+import pandas as pd
 from datetime import datetime, time
 
 def format_chainage(value):
@@ -9,6 +10,7 @@ def format_chainage(value):
   except:
     return "Invalid Input"
 
+st.set_page_config(page_title="PMNT Site Diary", page_icon="🛠️")
 st. title("PMNT Site Diary")
 
 # TEAM SELECTION
@@ -16,3 +18,10 @@ team = st.multiselect("TEAM:", ["TEAM A", "TEAM B", "TEAM C", "TEAM D", "TEAM E"
 
 # DATE SELECTION
 date_selected = st.date_input("Date:", datetime.today())
+
+# FORMAT DATE AS DD/MM/YY (DAY)
+formatted_date = date_selected.strftime("%d/%m/%y (%A)")
+
+# WEATHER SELECTION
+weather_am = st.selectbox("Morning Weather", ["Fine","Rainy"])
+weather_pm = st.selectbox("Afternoon Weather", ["Fine","Rainy"])
