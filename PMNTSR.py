91b3af1aa59337c9f10eecc56469002c06831c38
angelynelau <48088108+angelynelau@users.noreply.github.com>
2025-02-23 21:36:49 +0800

    Update PMNTSR.py

--- PMNTSR.py
+++ PMNTSR.py
@@ -18,10 +18,20 @@ team = st.multiselect("TEAM:", ["TEAM A", "TEAM B", "TEAM C", "TEAM D", "TEAM E"
 
 # DATE SELECTION
 date_selected = st.date_input("Date:", datetime.today())
-
-# FORMAT DATE AS DD/MM/YY (DAY)
 formatted_date = date_selected.strftime("%d/%m/%y (%A)")
 
 # WEATHER SELECTION
 weather_am = st.selectbox("Morning Weather", ["Fine","Rainy"])
 weather_pm = st.selectbox("Afternoon Weather", ["Fine","Rainy"])
+
+# WORKING HOURS
+start_time = st.time_input("Start Time:", time(8, 0))
+end_time = st.time_input("End Time:", time(17, 0))
+working_hours = ((datetime.combine(datetime.today(), end_time) - datetime.combine(datetime.today(), start_time)).seconds / 3600) - 1
+working_time = f"{start_time.strftime('%H%M')}-{end_time.strftime('%H%M')} hrs"
+
+data = []
+
+for team in teams:
+  st.subheader(f"{team}")
+  pipe_size = st.selectbox(f"{team} - Pipe Size", ["400mm HDPE", "355mm HDPE", "280mm HDPE", "225mm HDPE", "160mm HDPE"])
