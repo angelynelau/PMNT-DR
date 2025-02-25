bbd02f36cd583e63f04e0bf104b9175f4d7a536d
angelynelau <48088108+angelynelau@users.noreply.github.com>
2025-02-25 21:17:17 +0800

    Update PMNTSR.py

--- PMNTSR.py
+++ PMNTSR.py
@@ -136,6 +136,7 @@ if st.button("Generate Report"):
     pmnt_report = ""
 
     for _, row in edited_df.iterrows():
+        activity_text = f"WORK ACTIVITY = " & ".join(activity_list)"
         route_text = team_routes.get(row["Team"], "")
         laid_text = f"LAID = {route_text}-{row['Laid Start']} to {row['Laid End']} ({row['Laid Length(m)']})" if row["Laid Start"] or row["Laid End"] or row["Laid Length(m)"] else "LAID = "
         weather_text = f"WEATHER = {weather_am}" if weather_am == weather_pm else f"WEATHER = {weather_am} (am) / {weather_pm} (pm)"
