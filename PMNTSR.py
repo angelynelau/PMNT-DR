93ea48ee3b8c4941bf197cd20160dfa7c3de4676
angelynelau <48088108+angelynelau@users.noreply.github.com>
2025-02-25 17:56:22 +0800

    Update PMNTSR.py

--- PMNTSR.py
+++ PMNTSR.py
@@ -65,7 +65,7 @@ data.append([
     pipe_size if pipe_size else "", 
     activity_list if activity_list else "", 
     working_hours if working_hours else "", 
-    joints if "Pipe Jointing" in activity_list else "", 
+    joints if joints else "", 
     fittings if fittings else ""
 ])
 
