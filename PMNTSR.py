2b4b161bd1d6172c68e5716525abfd304aaa8b9c
angelynelau <48088108+angelynelau@users.noreply.github.com>
2025-02-26 21:29:05 +0800

    Update PMNTSR.py

--- PMNTSR.py
+++ PMNTSR.py
@@ -168,7 +168,7 @@ if st.button("Generate Report"):
     manpower_summary = {"Supervisor": 0, "Excavator Operator": 0, "General Worker": 0}
     material_summary = {}
     machinery_summary = {"Excavator": 0}
-    equipment_summary = {"Genset": 0, "Butt Fusion Welding Machine": 0}
+    equipment_summary = {}
 
 
     for _, row in edited_df.iterrows():
@@ -198,9 +198,10 @@ if st.button("Generate Report"):
 
     # Equipment
         if st.session_state.get(f"genset_{team}"):
-            equipment_summary["Genset"] += 1
+            equipment_summary["Genset"] = equipment_summary.get("Genset", 0) + 1
         if st.session_state.get(f"welding_{team}"):
-            equipment_summary["Butt Fusion Welding Machine"] += 1
+            equipment_summary["Butt Fusion Welding Machine"] = equipment_summary.get("Butt Fusion Welding Machine", 0) + 1
+
         
         # Generate delivery text
         delivery_text = "DELIVERY = "
