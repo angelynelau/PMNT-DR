14e6491badaca5dd2a8fee4574f93bed532c3e8d
angelynelau <48088108+angelynelau@users.noreply.github.com>
2025-02-26 19:43:53 +0800

    Update PMNTSR.py

--- PMNTSR.py
+++ PMNTSR.py
@@ -200,7 +200,21 @@ if st.button("Generate Report"):
             f"REMARKS = {row['Remarks']}\n"
             "\n"
         )
-    
+
+#amweather_text = f"Morning: {weather_am}"
+#pmweather_text = f"Afternoon: {weather_pm}"
+#totalworkinghours_text = f"Total Working Hours: {working_hours} hrs"
+#workingtime_text = f"{working_time}"
+
+# Build JBALB Report
+jbalb_report = (
+    f"Date: {formatted_date}\n"
+    f"Morning: {weather_am}\n"  
+    f"Afternoon: {weather_pm}\n"
+    f"{totalworkinghours_text}\n"   
+    f"{workingtime_text}\n"
+)
+
     jbalb_report = ""
 
     for _, row in edited_df.iterrows():
