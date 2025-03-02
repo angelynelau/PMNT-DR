f2afec6cd476ac8391cb64bad1140d966b81e6fb
angelynelau <48088108+angelynelau@users.noreply.github.com>
2025-03-02 19:36:59 +0800

    Update PMNTSR.py

--- PMNTSR.py
+++ PMNTSR.py
@@ -65,8 +65,8 @@ for team in teams:
     # ROUTE
     route = st.text_input("ROUTE:", key=f"route_{team}")
     route = validate_text_input(route)
-    if team and lroute:
-        team_routes[team] = lroute
+    if team and route:
+        team_routes[team] = route
         
     # ACTIVITY
     activity_list = st.multiselect("ACTIVITY CARRIED OUT:", ["Pipe Jointing", "Pipe Laying", "Road Reinstatement"], key=f"activity_{team}")
