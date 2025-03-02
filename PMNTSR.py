644b8261dcc0bc5d88d74de8aa065aa0283782b7
angelynelau <48088108+angelynelau@users.noreply.github.com>
2025-03-02 21:09:07 +0800

    Update PMNTSR.py

--- PMNTSR.py
+++ PMNTSR.py
@@ -216,15 +216,15 @@ if st.button("Generate Report"):
     manpower_summary = {"Supervisor": 0, "Excavator Operator": 0, "General Worker": 0}
 
     # MANPOWER SUMMARY
-for team in teams:
-    for member in team_manpower.get(team, {}).get("members", []):
-        if "Supervisor" in member:
-            manpower_summary["Supervisor"] += 1
-        elif "Excavator Operator" in member:  # Fixed typo from 'Excavator Supervisor'
-            manpower_summary["Excavator Operator"] += 1
-        elif "General Worker" in member:
-            count = int(re.search(r'\d+', member).group()) if re.search(r'\d+', member) else 1
-            manpower_summary["General Worker"] += count
+    for team in teams:
+        for member in team_manpower.get(team, {}).get("members", []):
+            if "Supervisor" in member:
+                manpower_summary["Supervisor"] += 1
+            elif "Excavator Operator" in member:  # Fixed typo from 'Excavator Supervisor'
+                manpower_summary["Excavator Operator"] += 1
+            elif "General Worker" in member:
+                count = int(re.search(r'\d+', member).group()) if re.search(r'\d+', member) else 1
+                manpower_summary["General Worker"] += count
 
     
     jbalb_report += (
