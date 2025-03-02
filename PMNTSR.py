ed1b1c810e285a899a74ced343e0044e9631b95b
angelynelau <48088108+angelynelau@users.noreply.github.com>
2025-03-02 20:53:56 +0800

    Update PMNTSR.py

--- PMNTSR.py
+++ PMNTSR.py
@@ -166,6 +166,7 @@ for team in teams:
         pipe_count = st.number_input(f"TOTAL NUMBER DELIVERED for {team}", min_value=0, step=1, key=f"pipe_count_{team}")
         delroute = st.text_input(f"ROUTE for {team}:", key=f"delroute_{team}")
         delroute = validate_text_input(delroute)
+        team_delivery[team] = pipe_count
     
     # REMARKS
     remarks = st.text_input("REMARKS:", key=f"remarks{team}")
@@ -195,7 +196,7 @@ if st.button("Generate Report"):
 
     for _, row in edited_df.iterrows():
         laid_text = f"{row['Laid Start']} to {row['Laid End']} ({row['Laid Length (m)']})" if row["Laid Start"] or row["Laid End"] or row["Laid Length (m)"] else ""
-        del_text = f"{row['Pipe Size']} - {row[pipe_count]} lengths" if pipe_count else ""
+        del_text = f"{row['Pipe Size']} - {team_delivery.get(row['Team'], 0)} lengths" if team_delivery.get(row['Team'], 0) else ""
         weather_text = f"{weather_am}" if weather_am == weather_pm else f"WEATHER = {weather_am} (am) / {weather_pm} (pm)"
         pmnt_report += (
             f"> {row['Team']} (ROUTE {team_routes.get(row['Team'])})\n"
