02c2101a2c7c9759d9a6d2c4cfb39755761b6992
angelynelau <48088108+angelynelau@users.noreply.github.com>
2025-03-02 20:50:26 +0800

    Update PMNTSR.py

--- PMNTSR.py
+++ PMNTSR.py
@@ -195,8 +195,8 @@ if st.button("Generate Report"):
 
     for _, row in edited_df.iterrows():
         laid_text = f"{row['Laid Start']} to {row['Laid End']} ({row['Laid Length (m)']})" if row["Laid Start"] or row["Laid End"] or row["Laid Length (m)"] else ""
-        del_text = f"{row['Pipe Size']} - {pipe_count} lengths" if pipe_count else ""
-        weather_text = f"WEATHER = {weather_am}" if weather_am == weather_pm else f"WEATHER = {weather_am} (am) / {weather_pm} (pm)"
+        del_text = f"{row['Pipe Size']} - {row[pipe_count]} lengths" if pipe_count else ""
+        weather_text = f"{weather_am}" if weather_am == weather_pm else f"WEATHER = {weather_am} (am) / {weather_pm} (pm)"
         pmnt_report += (
             f"> {row['Team']} (ROUTE {team_routes.get(row['Team'])})\n"
             f"PIPE = {row['Pipe Size']}\n"
