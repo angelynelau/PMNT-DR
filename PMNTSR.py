7bdd41e17361ecf40edc2ce13f2796dde9ef8fca
angelynelau <48088108+angelynelau@users.noreply.github.com>
2025-04-01 16:49:18 +0800

    Update PMNTSR.py

--- PMNTSR.py
+++ PMNTSR.py
@@ -136,7 +136,7 @@ for team in teams:
                         selected_data[fitting].append(fitting_ch)
         for fitting, chainages in selected_data.items():
             fitting_chainages.append(f"{fitting} ({', '.join(chainages)})")
-        team_activities[team] += {', '.join(fitting_chainages)}"
+        team_activities[team] += {', '.join(fitting_chainages)}
 
     # ROAD REINSTATEMENT
     ("**ROAD REINSTATEMENT:**") if "Road Reinstatement" in activity_list else ""
@@ -147,7 +147,7 @@ for team in teams:
     rrch_diff = ""
     if rrstartch_raw and rrendch_raw:
         try:
-            rrch_diff = f"{int(rrendch_raw) - int(rrstartch_raw)} m"
+            rrch_diff = f"{int(rrendch_raw) - int(rrstartch_raw)}m"
         except ValueError:
             rrch_diff = "Invalid"
     
