672beb44f8be772057ba3d2c8e159cbc45a6d239
angelynelau <48088108+angelynelau@users.noreply.github.com>
2025-04-01 16:47:57 +0800

    Update PMNTSR.py

--- PMNTSR.py
+++ PMNTSR.py
@@ -136,7 +136,7 @@ for team in teams:
                         selected_data[fitting].append(fitting_ch)
         for fitting, chainages in selected_data.items():
             fitting_chainages.append(f"{fitting} ({', '.join(chainages)})")
-        team_activities[team] += f"\nFITTING = {', '.join(fitting_chainages)}"
+        # team_activities[team] += f"\nFITTING = {', '.join(fitting_chainages)}"
 
     # ROAD REINSTATEMENT
     ("**ROAD REINSTATEMENT:**") if "Road Reinstatement" in activity_list else ""
